4062835f4342d97313bd1a7dd3979a1770843dcf
moxcelix <maxim.spf@gmail.com>
2026-02-13 13:24:49 +0300

    update analyzer fpr multiple mode

--- addcheck/addcheck.go
+++ addcheck/addcheck.go
@@ -7,13 +7,17 @@ import (
 	"go/token"
 	"go/types"
 
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+
 	"golang.org/x/tools/go/analysis"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "addlint",
-	Doc:  "reports integer additions",
-	Run:  run,
+	Name:     "addlint",
+	Doc:      "reports integer additions",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func render(fset *token.FileSet, x interface{}) string {
@@ -25,51 +29,51 @@ func render(fset *token.FileSet, x interface{}) string {
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	for _, file := range pass.Files {
-		ast.Inspect(file, func(n ast.Node) bool {
-			be, ok := n.(*ast.BinaryExpr)
-			if !ok {
-				return true
-			}
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
-			if be.Op != token.ADD {
-				return true
-			}
-
-			if _, ok := be.X.(*ast.BasicLit); !ok {
-				return true
-			}
-
-			if _, ok := be.Y.(*ast.BasicLit); !ok {
-				return true
-			}
-
-			isInteger := func(expr ast.Expr) bool {
-				t := pass.TypesInfo.TypeOf(expr)
-				if t == nil {
-					return false
-				}
-
-				bt, ok := t.Underlying().(*types.Basic)
-				if !ok {
-					return false
-				}
-
-				if (bt.Info() & types.IsInteger) == 0 {
-					return false
-				}
-
-				return true
-			}
-
-			if !isInteger(be.X) || !isInteger(be.Y) {
-				return true
-			}
-
-			pass.Reportf(be.Pos(), "integer addition found %q", render(pass.Fset, be))
-			return true
-		})
+	nodeFilter := []ast.Node{
+		(*ast.BinaryExpr)(nil),
 	}
+
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		be := n.(*ast.BinaryExpr)
+		if be.Op != token.ADD {
+			return
+		}
+
+		if _, ok := be.X.(*ast.BasicLit); !ok {
+			return
+		}
+
+		if _, ok := be.Y.(*ast.BasicLit); !ok {
+			return
+		}
+
+		isInteger := func(expr ast.Expr) bool {
+			t := pass.TypesInfo.TypeOf(expr)
+			if t == nil {
+				return false
+			}
+
+			bt, ok := t.Underlying().(*types.Basic)
+			if !ok {
+				return false
+			}
+
+			if (bt.Info() & types.IsInteger) == 0 {
+				return false
+			}
+
+			return true
+		}
+
+		if !isInteger(be.X) || !isInteger(be.Y) {
+			return
+		}
+
+		pass.Reportf(be.Pos(), "integer addition found %q",
+			render(pass.Fset, be))
+	})
 
 	return nil, nil
 }
